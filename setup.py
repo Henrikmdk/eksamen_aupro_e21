3ce31f3b188f814ed2dc8539b51f070828cfb00f
henrik <henrik.mortensen79@gmail.com>
2021-10-18 15:47:45 +0200

    Version 4 - database branch

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eksamen_aupro_e21',
+    version='v1.0.0-alpha',
+    packages=[''],
+    url='',
+    license='my property, dont replicate without my permision',
+    author='henrik',
+    author_email='henrik.mortensen79@gmail.com',
+    description='Eksamens release'
+)
